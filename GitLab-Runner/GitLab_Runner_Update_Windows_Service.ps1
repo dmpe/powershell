8bf9b1d23ce1d560a34d362acca0593c058ee0cf
John <cincenko@outlook.com>
2019-09-18 11:05:57 +0200

    Update GitLab_Runner_Update_Windows_Service.ps1

--- GitLab-Runner/GitLab_Runner_Update_Windows_Service.ps1
+++ GitLab-Runner/GitLab_Runner_Update_Windows_Service.ps1
@@ -1,7 +1,5 @@
 #requires -version 5.0
 
-### This runner is triggered by logging in the Windows Event Log and it updates GitLab runner.
-
 function StopExistingRunnerService {
     Get-Service -name "runner*" | Stop-Service -Verbose
 }
@@ -10,14 +8,23 @@ function StartExistingRunnerService {
     Get-Service -name "runner*" | Start-Service -Verbose
 }
 
-function RenameGLRunerFile{
+function RenameGLRunerFile {
     dir $alm_path
     Remove-Item -Path "$alm_path\$runner_name"
     # move and rename
     Move-Item -Path "$alm_path\runners\$last_gitlab_runner" -Destination "$alm_path\$runner_name" -Force
 }
 
-function ReplaceOldRunner {
+function Update-OldRunner {
+    <#
+    .SYNOPSIS
+        Replace current GitLab Runner exe with the newest one. Triggered by a Windows Task.
+    .DESCRIPTION
+        When the newest exe file is moved to the target directory, such an event is being logged in the Windows Event Log.
+        Up the log ("Information"), this script is started and updates GitLab runner for all runner services.
+    .LINK
+        https://mcpmag.com/articles/2015/09/24/changes-to-a-folder-using-powershell.aspx
+    #>
     [cmdletbinding()]
     Param (
         [parameter()]
@@ -27,7 +34,10 @@ function ReplaceOldRunner {
         [string]$runner_name = "gitlab-runner.exe",
 
         [parameter()]
-        [string]$runner_version_tag = "gitlab-runner-windows.recent"
+        [string]$runner_version_tag = "gitlab-runner-windows.recent",
+
+        [parameter()]
+        [string]$veto_file = "gitlab-runner-windows-veto.recent"
     )
 
     Begin {
@@ -35,11 +45,17 @@ function ReplaceOldRunner {
         Write-Host $msg
         Write-EventLog -LogName Application -Source "GitLab Runner Observer" -Message $msg -EventId 1
 
-        try {
-            $last_gitlab_runner = Get-Content -Path $alm_path\$runner_version_tag
-        } catch {
-            exit
+        if (Test-Path -Path $alm_path\$veto_file) {
+            $runner_version_tag = $veto_file
         }
+
+        $last_gitlab_runner = Get-Content -Path $alm_path\$runner_version_tag
+        # try {
+
+        # }
+        # catch {
+        #     exit
+        # }
     }
 
     Process {
@@ -63,4 +79,5 @@ function ReplaceOldRunner {
     }
 }
 
-ReplaceOldRunner
+# this executes the above function
+Update-OldRunner
